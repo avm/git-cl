d5539df4a441e17225d19acc4ed4e7bb6e1c1c82
Graham Percival <graham@percival-music.ca>
2011-09-26 02:55:44 +0100

    Refactor, auto-detect updating existing issue

--- projecthosting_upload.py
+++ projecthosting_upload.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 # API docs:
 #   http://code.google.com/p/support/wiki/IssueTrackerAPIPython
 import gdata.projecthosting.client
@@ -10,38 +12,96 @@ import gdata.data
 import atom.http_core
 import atom.core
 
-# TODO: handle this better
-my_username = "percival.music.ca"
-my_password = "hunter2"
-
-def authenticating_client(client, username, password):
-    return client.client_login(
-        username,
-        password,
-        source='lilypond-patch-handler',
-        service='code')
 
 
-def create_issue(client, subject, description):
-    """Create an issue."""
-    return client.add_issue(
-        "lilypond",
-        "Patch: " + subject,
-        description,
-        my_username,
-        labels=["Patch-new", "Type-Other"])
-        # default to Other?
-
-def upload(issue, patchset, subject="", description=""):
+class PatchBot():
     client = gdata.projecthosting.client.ProjectHostingClient()
-    # TODO: error checking
-    authenticating_client(client, my_username, my_password)
 
-    # TODO: check if existing patch via "fix 1234" or something
-    # like that.
-    if not subject:
-        subject = "new patch"
-    description = description + "\n\n" + "http://codereview.appspot.com/" + issue
-    create_issue(client, subject, description)
+    # you can use mewes for complete junk testing
+    #PROJECT_NAME = "mewes"
+    PROJECT_NAME = "lilypond"
+
+    username = None
+    password = None
+
+    def __init__(self):
+        # both of these bail if they fail
+        self.get_credentials()
+        self.login()
+
+    def get_credentials(self):
+        # TODO: handle this better
+        filename = "google.login"
+        try:
+            login_data = open(filename).readlines()
+            self.username = login_data[0]
+            self.password = login_data[1]
+        except:
+            print "Error: you must create a %s" % (filename),
+            print "file containing your username and password"
+            print "(one on each line)"
+            sys.exit(1)
+
+    def login(self):
+        try:
+            self.client.client_login(
+                self.username, self.password,
+                source='lilypond-patch-handler', service='code')
+        except:
+            print "Incorrect username or password"
+            sys.exit(1)
+
+
+    def create_issue(self, subject, description):
+        """Create an issue."""
+        return self.client.add_issue(
+            self.PROJECT_NAME,
+            "Patch: " + subject,
+            description,
+            self.username,
+            labels = ["Type-Enhancement", "Patch-new"])
+
+    def update_issue(self, issue_id, description):
+        return self.client.update_issue(
+            self.PROJECT_NAME,
+            issue_id,
+            self.username,
+            comment = description,
+            labels = ["Patch-new"])
+
+    def find_fix_issue_id(self, text):
+        splittext = text.split()
+        issue_id = None
+        # greedy search for the issue id
+        for i, word in enumerate(splittext):
+            if word == "fix" or word == "issue":
+                try:
+                    issue_id = int(splittext[i+1])
+                    break
+                except:
+                    pass
+        return issue_id
+
+    def upload(self, issue, patchset, subject="", description=""):
+        if not subject:
+            subject = "new patch"
+        description = description + "\n\n" + "http://codereview.appspot.com/" + issue
+        # update or create?
+        issue_id = self.find_fix_issue_id(subject+' '+description)
+        if issue_id:
+            self.update_issue(issue_id, description)
+        else:
+            self.create_issue(subject, description)
+        return True
+
+
+# hacky integration
+def upload(issue, patchset, subject="", description=""):
+    patchy = PatchBot()
+    status = patchy.upload(issue, patchset, subject, description)
+    if status:
+        print "Tracker issue done"
+    else:
+        print "Problem with the tracker issue"
 
 
